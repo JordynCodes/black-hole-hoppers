030b4dec90fcd5e351a37fd83c3c77032198cc16
Jordyn Lally <codingwithjordyn@gmail.com>
2025-05-04 21:27:06 -0400

    removed wildstacker support

--- src/main/java/me/jordyn/tasks/CollectItemsTask.java
+++ src/main/java/me/jordyn/tasks/CollectItemsTask.java
@@ -1,18 +1,11 @@
 package me.jordyn.tasks;
 
-import java.util.List;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Hopper;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import com.bgsoftware.wildstacker.api.WildStackerAPI;
-import com.bgsoftware.wildstacker.api.handlers.SystemManager;
-import com.bgsoftware.wildstacker.api.objects.StackedItem;
-import me.jordyn.configs.HopperData;
 
 public class CollectItemsTask extends BukkitRunnable{
 
